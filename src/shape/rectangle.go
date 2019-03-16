338bb0103e282e3f07f678cb91d3acaa3c867835
Giang <gn02.nguyen@gmail.com>
2019-03-16 14:29:48 -0700

    Initial Perimeter.

--- src/shape/rectangle.go
+++ src/shape/rectangle.go
@@ -1,7 +1,21 @@
 package main
 
-type rectangle float64
+import (
+	"fmt"
+)
 
+type rectangle struct {
+	width  float64
+	height float64
+}
+
+func (rec rectangle) perimeter() float64 {
+	return (rec.width + rec.height) * 2
+}
 func main() {
-
+	rec := rectangle{
+		width:  3,
+		height: 4,
+	}
+	fmt.Println("the perimeter of the rectangle is:", rec.perimeter())
 }
