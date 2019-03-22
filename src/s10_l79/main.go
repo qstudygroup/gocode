84cd6496a224c5d1816fc1baac05ca87c76c507e
Giang <gn02.nguyen@gmail.com>
2019-03-21 20:03:36 -0700

    Impement the program for section 10 lecture 79

--- src/s10_l79/main.go
+++ src/s10_l79/main.go
@@ -10,4 +10,12 @@ func main() {
 		"Moneypenny": 27,
 	}
 	fmt.Println(m)
+	fmt.Println(m["James"])
+
+	v, ok := m["Barnabas"]
+	fmt.Println(v, ok)
+
+	if v, ok := m["Moneypenny"]; ok {
+		fmt.Println("THIS IS THE IF PRINT", v)
+	}
 }
