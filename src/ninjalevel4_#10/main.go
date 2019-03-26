fdffe6f169243e401e591c72b4c23bd7728b5a5c
Giang <gn02.nguyen@gmail.com>
2019-03-25 20:06:52 -0700

    Implement level 4 Ninja program excercise 10.

---
+++ src/ninjalevel4_#10/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	lastFirst := map[string][]string{
+		"bond_james":      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		"moneypenny_miss": []string{`James Bond`, `Literature`, `Computer Science`},
+		"no_dr":           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	for k, v := range lastFirst {
+		fmt.Println("this is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+	lastFirst["doan_quang"] = []string{`Golden Retriever`, `Sour patch`, `annoy people`}
+	for k, v := range lastFirst {
+		fmt.Println("this is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+	delete(lastFirst, "doan_quang")
+	for k, v := range lastFirst {
+		fmt.Println("this is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+
+}
