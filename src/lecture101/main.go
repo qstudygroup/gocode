f052433f570fb5cfbbf1345eb23ada4edd8ec491
quang <quang.m.doan@gmail.com>
2019-03-27 21:51:11 -0700

    Implement program for lecture 101.

---
+++ src/lecture101/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+func main(){
+	foo()
+	bar("James")
+	s1 := woo("MonneyPenny")
+	fmt.Println(s1)
+	mouse()
+}		
+	func foo(){
+		fmt.Println("hello from foo")
+	}
+	func bar(s string) {
+		fmt.Println("Hello,", s)
+
+	}
+
+	func woo(st string) string {
+			return fmt.Sprint("Hello from woo,", st)
+	}
