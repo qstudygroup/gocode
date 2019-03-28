769d6c97d80b1bd790a9a828998c2693988b9b67
Giang <gn02.nguyen@gmail.com>
2019-03-27 21:42:49 -0700

    Implement codde form section 13 lecture 99.

---
+++ src/s13_l99/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle struct {
+	doors int
+	color string
+}
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+	t := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "blue",
+		},
+		fourWheel: true,
+	}
+	s := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "black",
+		},
+
+		luxury: false,
+	}
+	fmt.Println(t)
+	fmt.Println(s)
+
+	fmt.Println(t.doors, t.color, t.fourWheel)
+	fmt.Println(s.doors, s.color, s.luxury)
+}
