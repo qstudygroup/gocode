7afedeaf797019da3a42c711928d83415b2a8ae4
Giang Nguyen <gn02.nguyen@gmail.com>
2019-03-29 04:31:45 -0700

    Implement example program from lecture 105.

---
+++ src/l105/main.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"fmt"
+)
+
+type person struct{
+	first string
+	last string
+}
+
+type secretAgent struct{
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak(){
+	fmt.Println("I am", s.first, s.last)
+}
+
+func main(){
+	sa1:=secretAgent{
+		person : person{
+			first: "James",
+			last: "Bond",
+		},
+		ltk: true,
+	}
+
+	sa2:=secretAgent{
+		person : person{
+			first: "Perry",
+			last: "the Platypus",
+		},
+		ltk: true,
+	}
+
+	sa1.speak()
+	sa2.speak()
+
+}
