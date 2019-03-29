df1bafd6a35c66354091ed3f65e10278a2d46eab
Giang Nguyen <gn02.nguyen@gmail.com>
2019-03-29 04:29:14 -0700

    Implement example program from lecture 101.

--- src/s14_l101/main.go
+++ src/s14_l101/main.go
@@ -1,14 +1,21 @@
+// lecture 101
+// https://www.udemy.com/learn-how-to-code/learn/v4/t/lecture/11922226?start=630
+
+
 package main
 
 import (
 	"fmt"
 )
-
+//func (r receiver) identifier(parameteres) (return(r)) {...}
 func main() {
 	foo()
 	bar("James")
 	st := woo("Moneypenny")
 	fmt.Println(st)
+	x,y := mouse("Ian", "Fleming")
+	fmt.Print(x)
+	fmt.Println(y)
 }
 
 func foo() {
@@ -22,3 +29,10 @@ func bar(s string) {
 func woo(st string) string {
 	return fmt.Sprint("hello from woo, ", st)
 }
+
+func mouse(fn string, ln string) (string, bool){
+	a := fmt.Sprintln(fn, ln,`, says "Hello"`)
+	b := true
+	return a,b
+}
+ 
