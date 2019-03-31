6b0095466b32d244c421deeecedcadbb3126f9bb
Giang Nguyen <gn02.nguyen@gmail.com>
2019-03-30 22:25:47 -0700

    Implement chu Thanh code to count the odd numbers from 0 to 100.

--- src/d4/main.go
+++ src/d4/main.go
@@ -8,15 +8,12 @@ import (
 
 func main() {
 	count := 0
-	for x := 0; x <= 100; x++ {
-		s1 := rand.New(rand.NewSource(time.Now().UnixNano()))
-		s2 := s1.Intn(100)
-
-		if s2%2 == 1 {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for x := 0; x < 100; x++ {
+		num := r.Intn(100)
+		if num%2 == 1 {
 			count++
 		}
-		fmt.Println(s2)
 	}
 	fmt.Println("there are", count, "odd number")
-
 }
