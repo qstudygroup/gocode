7e8e1310f422bdc613bf835b442ed460c3ab276b
quang <quang.m.doan@gmail.com>
2019-04-05 18:56:03 -0700

    Implement looptheloop

--- src/looptheloop/loop.go
+++ src/looptheloop/loop.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 100; i++ {
-		fmt.Println(i)
+	s := 0
+	for i := 1; i <= 20; i++ {
+		if i%4 == 0 {
+			s += i
+		}
 	}
+	fmt.Println(s)
 }
