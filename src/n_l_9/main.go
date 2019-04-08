db72ec0f0b4cf3882dc8d01c678a8e26cdbda0b6
quang <quang.m.doan@gmail.com>
2019-04-07 19:14:18 -0700

    Implement n_l_9

---
+++ src/n_l_9/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	favsport := "videogames"
+	switch favsport {
+	case "videogames":
+		fmt.Println("go to the xbox")
+	case "surfing":
+		fmt.Println("go to hawaii")
+	case "soccer":
+		fmt.Println("go to the sand field soccer court")
+
+	}
+
+}
