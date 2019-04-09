ca3c59a55bfcd332c617956b0eda6cad5067bc24
quang <quang.m.doan@gmail.com>
2019-04-08 17:36:25 -0700

    Implement sliceslicing

---
+++ src/sliceslicing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{4, 5, 7, 8, 42}
+	fmt.Println(x[1])
+	fmt.Println(x)
+	fmt.Println(x[1:])
+	fmt.Println(x[1:3])
+
+	for i := 0; i <= 4; i++ {
+		fmt.Println(i, x[i])
+	}
+}
