5f128929da320e746d187f8d3cde9cf58ffb0e75
quang <quang.m.doan@gmail.com>
2019-04-11 15:55:39 -0700

    n_4_ex_6

---
+++ src/n_4_ex_6/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	xs1 := []string{"James", "Bond", "Shaken, Not stirred"}
+	xs2 := []string{"Miss", "Money Penny", "Helloooo, James."}
+	fmt.Println(xs1)
+	fmt.Println(xs2)
+
+	xxs := [][]string{xs1, xs2}
+	fmt.Println(xxs)
+
+	for i, xs := range xxs {
+		fmt.Println("record:", i)
+		for j, val := range xs {
+			fmt.Printf("\t index position: %v \t value: \t %v \n", j, val)
+		}
+	}
+}
