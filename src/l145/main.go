e792db0d7f7c6d4a272567505851e6168f46d446
Giang <gn02.nguyen@gmail.com>
2019-04-17 19:59:07 -0700

    Implement code from lecture 145 about race condition.

---
+++ src/l145/main.go
@@ -0,0 +1,35 @@
+// lecture 145
+//https://www.udemy.com/learn-how-to-code/learn/v4/t/lecture/11922324?start=0
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	counter := 0
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
