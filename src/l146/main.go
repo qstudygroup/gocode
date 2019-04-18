598e36274f43a243c481f3f9b776af274eb95e8d
Giang <gn02.nguyen@gmail.com>
2019-04-17 20:11:49 -0700

    Implement code from lecture 146 about using mutex to prevent data race.

---
+++ src/l146/main.go
@@ -0,0 +1,38 @@
+// lecture 146
+// https://www.udemy.com/learn-how-to-code/learn/v4/t/lecture/11922326?start=2
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	counter := 0
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
