d754f9546203a8c01c8d2e01049ca3dea06f3174
quang <quang.m.doan@gmail.com>
2019-04-17 19:49:43 -0700

    methods

---
+++ src/methods/main.go
@@ -0,0 +1,34 @@
+package main
+
+type person struct{
+	first string
+	last string
+}
+
+type secretAgent struct {
+	person
+	ltk
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+}
+func main(){
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+}
