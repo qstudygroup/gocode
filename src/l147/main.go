70a946826366ff3d19b6319c25f083fbd706b2be
Giang <gn02.nguyen@gmail.com>
2019-04-18 18:48:44 -0700

    Implement code from lecture 147 about using atomic to write, load or read from an int64.

---
+++ src/l147/main.go
@@ -0,0 +1,35 @@
+// lecture 147
+//https://www.udemy.com/learn-how-to-code/learn/v4/t/lecture/11922328?start=0
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	var counter int64
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			fmt.Println("Counter \t:", atomic.LoadInt64(&counter))
+			runtime.Gosched()
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
